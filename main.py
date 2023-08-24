89f82e7dea126d4ca8b48666506cc5ca89cade77
莫颜JDC <1561515308@qq.com>
2023-08-24 17:43:52 +0800

    🎈 perf(main): 增加提权

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import webview
 import click
 from server import app as flask
-from loguru import logger as log
-import faulthandler as f
-
-f.dump_traceback(open("./1.txt", "w"), True)
-log.add(
-    "log/main.log",
-    rotation="1 days",
-    retention="7 days",
-    level="DEBUG",
-    compression="zip",
-)
-
 
 # 创建WebView窗口
 @click.command()
 @click.option("-d", "--debug", type=str, default="no", help="是否开启调试模式（yes/no）")
 def main(debug):
-    if debug == "yes":
-        log.debug("开启调试模式")
-        webview.create_window(
-            "HoYoGameLauncher",
-            url="http://127.0.0.1:6553",
-            text_select=True,
-            width=1280,
-            height=720,
-        )
-        webview.start(debug=True, user_agent="HoYoGameLauncher-WebView/1.0.0")
+    import ctypes, sys
+
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+    if is_admin():
+        # 将要运行的代码加到这里
+        if debug == "yes":
+            webview.create_window(
+                "HoYoGameLauncher",
+                url="http://127.0.0.1:6553",
+                text_select=True,
+                width=1280,
+                height=720,
+            )
+            webview.start(debug=True, user_agent="HoYoGameLauncher-WebView/1.0.0")
+        else:
+            webview.create_window(
+                "HoYoGameLauncher", url=flask, text_select=True, width=1280, height=720
+            )
+            webview.start(user_agent="HoYoGameLauncher-WebView/1.0.0")
     else:
-        log.debug("未使用调试模式")
-        webview.create_window(
-            "HoYoGameLauncher", url=flask, text_select=True, width=1280, height=720
-        )
-        webview.start(user_agent="HoYoGameLauncher-WebView/1.0.0")
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        
+    
 
 
 if __name__ == "__main__":
