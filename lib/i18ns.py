3f19859773b28746e6b45438b4367e510a60a96d
莫颜JDC <1561515308@qq.com>
2023-09-08 23:03:02 +0800

    🐞 fix(i18n): 修改i18n逻辑

--- lib/i18ns.py
+++ lib/i18ns.py
@@ -3,7 +3,9 @@ i18n.set('file_format', 'json')
 i18n.set('locale', 'zh-CN')
 i18n.set('fallback', 'zh-CN')
 i18n.set("filename_format","{locale}.{format}")
+i18n.set("enable_memoization",True)
 i18n.load_path.append('data/language')
+
 def t(key,*args,**kwargs):
     return i18n.t(key,*args,**kwargs)
     
